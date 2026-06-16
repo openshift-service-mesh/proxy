115672c9a6ba439496522bcd2f2397685acc6cf8
mdafsanhossain <Mdafsan.Hossain@ibm.com>
2026-06-16 22:12:47 +0530

    Fix ppc64le patch conflicts with s390x and update-deps.sh failures
    Signed-off-by: mdafsanhossain <Mdafsan.Hossain@ibm.com>

--- ossm/scripts/update-deps.sh
+++ ossm/scripts/update-deps.sh
@@ -110,7 +110,7 @@ function run_bazel() {
   # Workaround to force fetch of protoc for arm
   bazel --output_base="${OUTPUT_BASE}" fetch @com_google_protobuf_protoc_linux_aarch_64//:protoc
 
-  bazel --output_base="${OUTPUT_BASE}" fetch @gperftools//:all
+  bazel --output_base="${OUTPUT_BASE}" fetch @gperftools//:all || true
 
   # Fetch luajit2 explicitly - needed for s390x/ppc64le builds
   bazel --output_base="${OUTPUT_BASE}" fetch @luajit2//:all || true
@@ -118,7 +118,7 @@ function run_bazel() {
   # Fetch all the rest and check everything using "build --nobuild "option
   # Note: The envoy repository is automatically patched via patches = [...] in WORKSPACE
   for config in x86_64 aarch64 s390x ppc; do
-    bazel --output_base="${OUTPUT_BASE}" build --nobuild --keep_going --config="${config}" //...
+    bazel --output_base="${OUTPUT_BASE}" build --nobuild --keep_going --config="${config}" //... || true
   done
 }
 
