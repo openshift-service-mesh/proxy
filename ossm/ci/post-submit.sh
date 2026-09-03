a32770f1043bbb5a081a9722f884e27376184614
Jose Luis Ojosnegros <jojosneg@redhat.com>
2026-09-03 10:13:55 +0200

    ci: add .fmf metadata and fix post-submit.sh for Testing Farm (#402)
    Signed-off-by: Jose Luis Ojosnegros Manchón <jojosneg@redhat.com>

--- ossm/ci/post-submit.sh
+++ ossm/ci/post-submit.sh
@@ -10,15 +10,20 @@ source "${DIR}/common.sh"
 GCS_PROJECT=${GCS_PROJECT:-maistra-prow-testing}
 ARTIFACTS_GCS_PATH=${ARTIFACTS_GCS_PATH:-gs://maistra-prow-testing/proxy}
 
-gcloud auth activate-service-account --key-file="${GOOGLE_APPLICATION_CREDENTIALS}"
-gcloud config set project "${GCS_PROJECT}"
-
 # Build Envoy
 bazel_build envoy_tar
 
 # Copy artifacts to GCS
 SHA="$(git rev-parse --verify HEAD)"
 
-# Envoy
-gsutil cp bazel-bin/envoy_tar.tar.gz "${ARTIFACTS_GCS_PATH}/envoy-alpha-${SHA}.tar.gz"
-gsutil cp "${ARTIFACTS_GCS_PATH}/envoy-alpha-${SHA}.tar.gz" "${ARTIFACTS_GCS_PATH}/envoy-centos-alpha-${SHA}.tar.gz"
+ARTIFACT="envoy-alpha-${SHA}.tar.gz"
+cp -L bazel-bin/envoy_tar.tar.gz "${ARTIFACT}"
+
+# Upload to GCS (skipped when SKIP_GCS_UPLOAD=true, e.g. in Testing Farm context)
+if [[ "${SKIP_GCS_UPLOAD:-false}" != "true" ]]; then
+  gcloud auth activate-service-account \
+    --key-file="${GOOGLE_APPLICATION_CREDENTIALS:?GOOGLE_APPLICATION_CREDENTIALS must be set for GCS upload}"
+  gcloud config set project "${GCS_PROJECT}"
+  gsutil cp "${ARTIFACT}" "${ARTIFACTS_GCS_PATH}/${ARTIFACT}"
+  gsutil cp "${ARTIFACTS_GCS_PATH}/${ARTIFACT}" "${ARTIFACTS_GCS_PATH}/envoy-centos-alpha-${SHA}.tar.gz"
+fi
